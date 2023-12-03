a8bd3c2d7b6f165c1cb7dce960131ee8a65a523f
Dmytro <vasyuk.dima27@gmail.com>
2023-12-04 00:16:50 +0200

    Add Auth Endpoint

--- api/login_handler.py
+++ api/login_handler.py
@@ -1,13 +1,14 @@
 from datetime import timedelta
-from typing import Union
+from typing import Union, Annotated
 
 from fastapi import APIRouter, Depends, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import status
 
 import settings
-from api.schemas import Token
+from api.schemas import Token, TokenData
 from db.dals import Dal
 from db.models import User
 from db.session import get_db
@@ -15,6 +16,29 @@ from hashing import Hasher
 from security import create_access_token
 
 login_router = APIRouter()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
+
+
+async def get_current_user_from_token(token: str = Depends(oauth2_scheme),
+                                      db: AsyncSession = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = await _get_user_by_username_for_auth(username=username, db=db)
+    if user is None:
+        raise credentials_exception
+    return user
 
 
 async def _get_user_by_username_for_auth(username: str, db: AsyncSession):
@@ -41,7 +65,7 @@ async def authenticate_user(
 # takes username and password from OAuth2Form - if user registered - token response
 @login_router.post("/token", response_model=Token)
 async def login_for_access_token(
-    form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)
+        form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)
 ):
     user = await authenticate_user(form_data.username, form_data.password, db)
     if not user:
@@ -51,7 +75,14 @@ async def login_for_access_token(
         )
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.email, "other_custom_data": [1, 2, 3, 4]},
+        data={"sub": user.username, },
         expires_delta=access_token_expires,
     )
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@login_router.get("/me")
+async def read_users_me(
+        current_user: User = Depends(get_current_user_from_token),
+):
+    return {"Success": True, "current_user": current_user}
--- api/schemas.py
+++ api/schemas.py
@@ -25,3 +25,7 @@ class UserShow(UserBase):
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
